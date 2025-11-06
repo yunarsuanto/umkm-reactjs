820c9d29503fb0673fce3ff27c7ab7b961eb57d6
Fadhli Madjid <m.fadhlimadjid@gmail.com>
2025-11-07 00:58:53 +0700

    deployments

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Konfigurasi agar alias dan environment variable tetap seperti CRA
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: '/src',
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: '/src',
+        '@': '/src',
+      },
     },
-  },
-  server: {
-    port: 5173,
-    open: true,
-    // pastikan fallback ke index.html untuk react-router
-    fs: {
-      allow: ["."],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+      open: true,
+      // pastikan fallback ke index.html untuk react-router
+      fs: {
+        allow: ["."],
+      },
     },
-  },
+  }
 });
